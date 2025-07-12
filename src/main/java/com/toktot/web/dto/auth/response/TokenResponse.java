c3a2bd3effefb3bda6ae30145ead7a3e8c01fb47
ddangme <ssilver121@naver.com>
2025-07-13 06:09:32 +0900

    feat(dto): TokenResponse null 값 처리 개선

--- src/main/java/com/toktot/web/dto/auth/response/TokenResponse.java
+++ src/main/java/com/toktot/web/dto/auth/response/TokenResponse.java
@@ -1,7 +1,9 @@
 package com.toktot.web.dto.auth.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record TokenResponse(
         @JsonProperty("access_token")
         String accessToken,
