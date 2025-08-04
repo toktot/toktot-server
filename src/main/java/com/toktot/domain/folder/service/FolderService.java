8ab421d95536368c4d5f466436e2b8921f964d71
ddangme <ssilver121@naver.com>
2025-08-04 23:13:55 +0900

    feat(folder): Implement folder list retrieval rest api
    폴더 리스트 조회 REST API구현

--- src/main/java/com/toktot/domain/folder/service/FolderService.java
+++ src/main/java/com/toktot/domain/folder/service/FolderService.java
@@ -10,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Slf4j
 @Service
 @Transactional(readOnly = true)
@@ -21,10 +23,13 @@ public class FolderService {
 
     @Transactional
     public FolderResponse createFolder(User user, String folderName) {
-
         Folder folder = Folder.create(user, folderName);
         folderRepository.save(folder);
 
         return FolderResponse.from(folder);
+    }
+
+    public List<FolderResponse> ReadFolders(User user) {
+        return folderRepository.findFoldersWithReviewCountByUserId(user.getId());
     }
 }
--- src/main/java/com/toktot/web/controller/folder/FolderController.java
+++ src/main/java/com/toktot/web/controller/folder/FolderController.java
@@ -10,10 +10,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -35,6 +34,20 @@ public class FolderController {
                 .log();
 
         FolderResponse response = folderService.createFolder(user, request.folderName());
+
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<FolderResponse>>> readFolders(
+            @AuthenticationPrincipal User user) {
+
+        log.atInfo()
+                .setMessage("Review foloder read request received")
+                .addKeyValue("userId", user.getId())
+                .log();
+
+        List<FolderResponse> response = folderService.ReadFolders(user);
 
         return ResponseEntity.ok(ApiResponse.success(response));
     }
