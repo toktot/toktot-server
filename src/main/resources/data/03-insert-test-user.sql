e56b515bb8347822cbfc9fef5b4a308d519e8539
ddangme <ssilver121@naver.com>
2025-08-12 23:54:45 +0900

    chore(data): fix user password

--- src/main/resources/data/03-insert-test-user.sql
+++ src/main/resources/data/03-insert-test-user.sql
@@ -10,16 +10,17 @@ INSERT INTO users (
     created_at,
     updated_at
 ) VALUES
-      ('test1@toktot.com', '$2a$10$N9qo8uLOickgx2ZMRg.opu.7afdnkmRW8e8Y4qj2j0kxK8J/jH/L6', 'EMAIL', '테스트사용자1', 0, false, NULL, 0, NOW(), NOW()),
-      ('test2@toktot.com', '$2a$10$N9qo8uLOickgx2ZMRg.opu.7afdnkmRW8e8Y4qj2j0kxK8J/jH/L6', 'EMAIL', '테스트사용자2', 0, false, NULL, 0, NOW(), NOW()),
-      ('test3@toktot.com', '$2a$10$N9qo8uLOickgx2ZMRg.opu.7afdnkmRW8e8Y4qj2j0kxK8J/jH/L6', 'EMAIL', '테스트사용자3', 0, false, NULL, 0, NOW(), NOW()),
-      ('test4@toktot.com', '$2a$10$N9qo8uLOickgx2ZMRg.opu.7afdnkmRW8e8Y4qj2j0kxK8J/jH/L6', 'EMAIL', '테스트사용자4', 0, false, NULL, 0, NOW(), NOW()),
-      ('test5@toktot.com', '$2a$10$N9qo8uLOickgx2ZMRg.opu.7afdnkmRW8e8Y4qj2j0kxK8J/jH/L6', 'EMAIL', '테스트사용자5', 0, false, NULL, 0, NOW(), NOW()),
-      ('test6@toktot.com', '$2a$10$N9qo8uLOickgx2ZMRg.opu.7afdnkmRW8e8Y4qj2j0kxK8J/jH/L6', 'EMAIL', '테스트사용자6', 0, false, NULL, 0, NOW(), NOW()),
-      ('test7@toktot.com', '$2a$10$N9qo8uLOickgx2ZMRg.opu.7afdnkmRW8e8Y4qj2j0kxK8J/jH/L6', 'EMAIL', '테스트사용자7', 0, false, NULL, 0, NOW(), NOW()),
-      ('test8@toktot.com', '$2a$10$N9qo8uLOickgx2ZMRg.opu.7afdnkmRW8e8Y4qj2j0kxK8J/jH/L6', 'EMAIL', '테스트사용자8', 0, false, NULL, 0, NOW(), NOW()),
-      ('test9@toktot.com', '$2a$10$N9qo8uLOickgx2ZMRg.opu.7afdnkmRW8e8Y4qj2j0kxK8J/jH/L6', 'EMAIL', '테스트사용자9', 0, false, NULL, 0, NOW(), NOW()),
-      ('test10@toktot.com', '$2a$10$N9qo8uLOickgx2ZMRg.opu.7afdnkmRW8e8Y4qj2j0kxK8J/jH/L6', 'EMAIL', '테스트사용자10', 0, false, NULL, 0, NOW(), NOW());
+      ('test@toktot.com', '$2a$10$qY6Vt7tYeD6jqkIp5Tv8mO/Ps6tkdhGDa20mw0v3pFv7OIBx6ZGsq', 'EMAIL', '테스트사용자1', 0, false, NULL, 0, NOW(), NOW()),
+      ('test1@toktot.com', '$2a$10$qY6Vt7tYeD6jqkIp5Tv8mO/Ps6tkdhGDa20mw0v3pFv7OIBx6ZGsq', 'EMAIL', '테스트사용자1', 0, false, NULL, 0, NOW(), NOW()),
+      ('test2@toktot.com', '$2a$10$qY6Vt7tYeD6jqkIp5Tv8mO/Ps6tkdhGDa20mw0v3pFv7OIBx6ZGsq', 'EMAIL', '테스트사용자2', 0, false, NULL, 0, NOW(), NOW()),
+      ('test3@toktot.com', '$2a$10$qY6Vt7tYeD6jqkIp5Tv8mO/Ps6tkdhGDa20mw0v3pFv7OIBx6ZGsq', 'EMAIL', '테스트사용자3', 0, false, NULL, 0, NOW(), NOW()),
+      ('test4@toktot.com', '$2a$10$qY6Vt7tYeD6jqkIp5Tv8mO/Ps6tkdhGDa20mw0v3pFv7OIBx6ZGsq', 'EMAIL', '테스트사용자4', 0, false, NULL, 0, NOW(), NOW()),
+      ('test5@toktot.com', '$2a$10$qY6Vt7tYeD6jqkIp5Tv8mO/Ps6tkdhGDa20mw0v3pFv7OIBx6ZGsq', 'EMAIL', '테스트사용자5', 0, false, NULL, 0, NOW(), NOW()),
+      ('test6@toktot.com', '$2a$10$qY6Vt7tYeD6jqkIp5Tv8mO/Ps6tkdhGDa20mw0v3pFv7OIBx6ZGsq', 'EMAIL', '테스트사용자6', 0, false, NULL, 0, NOW(), NOW()),
+      ('test7@toktot.com', '$2a$10$qY6Vt7tYeD6jqkIp5Tv8mO/Ps6tkdhGDa20mw0v3pFv7OIBx6ZGsq', 'EMAIL', '테스트사용자7', 0, false, NULL, 0, NOW(), NOW()),
+      ('test8@toktot.com', '$2a$10$qY6Vt7tYeD6jqkIp5Tv8mO/Ps6tkdhGDa20mw0v3pFv7OIBx6ZGsq', 'EMAIL', '테스트사용자8', 0, false, NULL, 0, NOW(), NOW()),
+      ('test9@toktot.com', '$2a$10$qY6Vt7tYeD6jqkIp5Tv8mO/Ps6tkdhGDa20mw0v3pFv7OIBx6ZGsq', 'EMAIL', '테스트사용자9', 0, false, NULL, 0, NOW(), NOW()),
+      ('test10@toktot.com', '$2a$10$qY6Vt7tYeD6jqkIp5Tv8mO/Ps6tkdhGDa20mw0v3pFv7OIBx6ZGsq', 'EMAIL', '테스트사용자10', 0, false, NULL, 0, NOW(), NOW());
 
 select * from users;
 select * from reviews;
