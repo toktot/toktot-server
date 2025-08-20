659d7a36083de268574b1b3aa50bcf0daf2a0ffb
ddangme <ssilver121@naver.com>
2025-08-21 07:08:52 +0900

    refactor: Convert KakaoMeta to record and remove unused sameName field

--- src/main/java/com/toktot/external/kakao/dto/response/KakaoMeta.java
+++ src/main/java/com/toktot/external/kakao/dto/response/KakaoMeta.java
@@ -1,39 +1,16 @@
 package com.toktot.external.kakao.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-public class KakaoMeta {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record KakaoMeta(
+        @JsonProperty("total_count")
+        Integer totalCount,
 
-    @JsonProperty("total_count")
-    private Integer totalCount;
+        @JsonProperty("pageable_count")
+        Integer pageableCount,
 
-    @JsonProperty("pageable_count")
-    private Integer pageableCount;
-
-    @JsonProperty("is_end")
-    private Boolean isEnd;
-
-    @JsonProperty("same_name")
-    private KakaoSameName sameName;
-
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    public static class KakaoSameName {
-
-        @JsonProperty("region")
-        private String[] region;
-
-        @JsonProperty("keyword")
-        private String keyword;
-
-        @JsonProperty("selected_region")
-        private String selectedRegion;
-    }
-}
+        @JsonProperty("is_end")
+        Boolean isEnd
+) {}
