2dcbffa2b15c2ea7ce1389a982bb7fdfc86bdd28
ddangme <ssilver121@naver.com>
2025-08-21 13:45:23 +0900

    refactor: move Kakao search logic from Controller to Service layer

--- src/main/java/com/toktot/web/controller/restaurant/RestaurantSearchController.java
+++ src/main/java/com/toktot/web/controller/restaurant/RestaurantSearchController.java
@@ -1,13 +1,11 @@
 package com.toktot.web.controller.restaurant;
 
 import com.toktot.domain.restaurant.service.RestaurantSearchService;
-import com.toktot.external.kakao.KakaoApiConstants;
-import com.toktot.external.kakao.dto.request.KakaoPlaceSearchRequest;
-import com.toktot.external.kakao.dto.response.KakaoPlaceSearchResponse;
-import com.toktot.external.kakao.service.KakaoMapService;
+import com.toktot.external.kakao.dto.request.RestaurantSearchRequest;
 import com.toktot.web.dto.ApiResponse;
 import com.toktot.web.dto.restaurant.response.RestaurantSearchResponse;
 import com.toktot.web.dto.restaurant.response.RestaurantSearchResponseTemp;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -22,26 +20,19 @@ import java.util.List;
 public class RestaurantSearchController {
 
     private final RestaurantSearchService restaurantSearchService;
-    private final KakaoMapService kakaoMapService;
 
     @GetMapping("/search")
     public ResponseEntity<ApiResponse<List<RestaurantSearchResponseTemp>>> searchRestaurants() {
         List<RestaurantSearchResponseTemp> response = restaurantSearchService.getRestaurantResponse();
-
         return ResponseEntity.ok(ApiResponse.success(response));
     }
 
     @PostMapping("/kakao/search")
-    public ResponseEntity<ApiResponse<RestaurantSearchResponse>> searchRestaurantsKakao(@RequestBody KakaoPlaceSearchRequest request) {
-        log.info("request={}", request.toString());
-        KakaoPlaceSearchResponse response = kakaoMapService.searchJejuAllFoodAndCafePlace(request);
-        while(response.getPlaceInfos().size() < KakaoApiConstants.DEFAULT_SIZE && response.hasMorePages()) {
-            request = request.plusPage();
-            KakaoPlaceSearchResponse newResponse = kakaoMapService.searchJejuAllFoodAndCafePlace(request);
-            response = newResponse.addPlaceInfo(response);
-        }
+    public ResponseEntity<ApiResponse<RestaurantSearchResponse>> searchRestaurantsFromKakao(
+            @Valid @RequestBody RestaurantSearchRequest request) {
+        log.info("Kakao restaurant search request: {}", request);
 
-        RestaurantSearchResponse result = RestaurantSearchResponse.from(response, request.page());
+        RestaurantSearchResponse result = restaurantSearchService.searchFromKakaoWithPagination(request);
         return ResponseEntity.ok(ApiResponse.success(result));
     }
 }
