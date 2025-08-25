c7cb5946cf145b472ca70262d21d1badf3dd55e4
ddangme <ssilver121@naver.com>
2025-08-25 15:52:41 +0900

    feat: add TourAPI DetailIntro client implementation

--- src/main/java/com/toktot/external/tourapi/dto/TourApiDetailIntro.java
+++ src/main/java/com/toktot/external/tourapi/dto/TourApiDetailIntro.java
@@ -3,56 +3,24 @@ package com.toktot.external.tourapi.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record TourApiDetailIntro(
-        @JsonProperty("contentid")
-        String contentId,
-
-        @JsonProperty("contenttypeid")
-        String contentTypeId,
-
-        @JsonProperty("discountinfofood")
-        String discountInfo,
-
-        @JsonProperty("firstmenu")
-        String firstMenu,
-
-        @JsonProperty("infocenterfood")
-        String infoCenter,
-
-        @JsonProperty("kidsfacility")
-        String kidsFacility,
-
-        @JsonProperty("opendatefood")
-        String openDate,
-
-        @JsonProperty("opentimefood")
-        String openTime,
-
-        @JsonProperty("packing")
-        String packing,
-
-        @JsonProperty("parkingfood")
-        String parking,
-
-        @JsonProperty("reservationfood")
-        String reservation,
-
-        @JsonProperty("restdatefood")
-        String restDate,
-
-        @JsonProperty("scalefood")
-        String scale,
-
-        @JsonProperty("seat")
-        String seat,
-
-        @JsonProperty("smoking")
-        String smoking,
-
-        @JsonProperty("treatmenu")
-        String treatMenu,
-
-        @JsonProperty("lcnsno")
-        String licenseNumber
+        @JsonProperty("contentid") String contentId,
+        @JsonProperty("contenttypeid") String contentTypeId,
+        @JsonProperty("seat") String seat,
+        @JsonProperty("kidsfacility") String kidsFacility,
+        @JsonProperty("firstmenu") String firstMenu,
+        @JsonProperty("treatmenu") String treatMenu,
+        @JsonProperty("smoking") String smoking,
+        @JsonProperty("packing") String packing,
+        @JsonProperty("infocenterfood") String infoCenterFood,
+        @JsonProperty("scalefood") String scaleFood,
+        @JsonProperty("parkingfood") String parkingFood,
+        @JsonProperty("opendatefood") String openDateFood,
+        @JsonProperty("opentimefood") String openTimeFood,
+        @JsonProperty("restdatefood") String restDateFood,
+        @JsonProperty("discountinfofood") String discountInfoFood,
+        @JsonProperty("chkcreditcardfood") String chkCreditCardFood,
+        @JsonProperty("reservationfood") String reservationFood,
+        @JsonProperty("lcnsno") String licenseNo
 ) {
 
 }
